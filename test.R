749e16344854da00587de3fde25ed85d31468666
Christoffer Nordenlöw <63145499+cnordenlow@users.noreply.github.com>
2020-06-24 15:00:33 +0200

    Update test.R

--- test.R
+++ test.R
@@ -2,24 +2,39 @@
 
 
 test <- data_jobless_claims3 %>%
-  filter(rptdate > Sys.Date() - (365*3))%>%
-  group_by(state_group, rptdate)%>%
-  summarise(
-    jobless = sum(jobless_claims),
-    continuing_claims = sum(continuing_claims))
-
-
-
-test <- data_jobless_claims3 %>%
   filter(country == 1)%>%
-  filter(rptdate > Sys.Date() - 365)
+  filter(rptdate > Sys.Date() - 365)%>%
+  mutate(
+    'Initial Jobess  Claims' = jobless_claims,
+    'Continuing Jobess  Claims' = continuing_claims,
+             )
 
 
 test2 <- test%>%
-  select(rptdate, jobless_claims, continuing_claims, initial_claims_relative_pop, continuing_claims_relative_pop)%>%
-  gather(variable, value, initial_claims_relative_pop:continuing_claims_relative_pop)
+  select(rptdate, 'Initial Jobess  Claims' , 'Continuing Jobess  Claims', initial_claims_relative_pop, continuing_claims_relative_pop)%>%
+  gather(variable, value, 'Initial Jobess  Claims':'Continuing Jobess  Claims')
 
-p1 <- ggplot() + geom_line(aes(y = value, x = rptdate, colour = variable),
-                           data = test2, stat="identity")
 
-p1
+
+fig <- plot_ly(data = test2, x = ~rptdate, y = ~value, color = ~variable, symbol = ~variable,
+               colors = 'Purple', type = 'scatter', mode = 'lines',
+               
+               text = ~paste('<br>',"Initial Claims: ", '<br>Continuing Claims:'))%>%
+  layout(
+  title = "Initial Jobless Claims and Continuing Jobless Claims",
+  xaxis = list(title = "Date", gridcolor="grey"),
+  yaxis = list(title = "Claims",gridcolor="grey")
+  
+  )%>%
+  
+   # xaxis = list(title = "Continuing Claims", range=c(0, max(data_jobless_claims3$continuing_claims_relative_pop))),
+    #yaxis = list(title = "Initial Jobless Claims", range=c(0, max(data_jobless_claims3$initial_claims_relative_pop))))%>%
+  layout(legend = list(orientation = "h",xanchor = "center", x = 0.4, y = -0.2))%>%
+  hide_colorbar() %>%
+  plotly::config(displayModeBar = F)%>%
+  layout(annotations = 
+           list(x = 1, y = -0.1, text = "Source: US Department of Labor", 
+                showarrow = F, xref='paper', yref='paper', 
+                xanchor='right', yanchor='auto', xshift=0, yshift=0,
+                font=list(size=10, color="black")))
+  fig
